189ddd2dde0457fdc05fb4886062ffcd322a2f2b
Lijo Jolly <lijojolly10@gmail.com>
2024-02-28 23:42:12 +0530

    Add files via upload

---
+++ Square Of Integer Numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    float number,square;
+    printf("Enter any number");
+    scanf("%f", &number);
+    square= number* number;
+    printf("The square of %f is %f",number,square);
+
+}
